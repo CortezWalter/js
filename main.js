6f4920c4f941701d362f49a3d1cb5fcafed766c0
Walter <cortezwaltera@gmail.com>
2022-07-31 19:43:43 -0300

    Primera clase

---
+++ main.js
@@ -0,0 +1,18 @@
+let usuario = prompt("Ingresa usuario")
+let pass = prompt("Ingresa la contraseña")
+
+let user = usuario.toLowerCase()
+
+alert("El usuario ingresado es " + user + " y la contraseña es " + pass)
+
+console.log("Usuario y contraseña ingresados con exito")
+console.log(user)
+
+
+
+
+
+
+
+
+
